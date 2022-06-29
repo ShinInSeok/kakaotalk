6b079ff45f527bc3726bee5454f4bbf020611974
ShinInSeok <is7170@naver.com>
2022-06-29 14:54:30 +0900

    first commit

---
+++ js/kakao.js
@@ -0,0 +1,98 @@
+// const list =[{
+//     name : "석이",
+//     message : "말리지마",
+//     music : "하루하루 - 빅뱅",
+//     profile : "./CSS/IMG/seok.png",
+//     birthday : "19970327",
+//     type : "mine"
+// },
+// {
+//     name : "카카오 채널",
+//     message : "",
+//     music : "",
+//     profile : "./CSS/IMG/chanel.png",
+//     birthday : "",
+//     type : "channel"
+// },
+// {
+//     name : "김강희",
+//     message : "고독하구만",
+//     music : "Ibiza - tyga",
+//     profile : "./CSS/IMG/default.png",
+//     birthday : "19970127",
+//     type : "friends"
+// },
+// {
+//     name : "김효원",
+//     message : "행복하세요",
+//     music : "그리워그리워 - 노을",
+//     profile : "./CSS/IMG/kimhyo.png",
+//     birthday : "19980223",
+//     type : "friends"
+// },
+// {
+//     name : "권성수",
+//     message : "헬창의길..",
+//     music : "we here 2 - 일리네어 레코즈",
+//     profile : "./CSS/IMG/kwon.png",
+//     birthday : "19970731",
+//     type : "friends"
+// },
+// {
+//     name : "박도희",
+//     message : "여행가쟈!",
+//     music : "안녕나야 - 포맨",
+//     profile : "./CSS/IMG/parkdo.png",
+//     birthday : "19970814",
+//     type : "friends"
+// },
+// {
+//     name : "박기중",
+//     message : "자유롭고 싶다..",
+//     music : "어쩌라고 - 슈퍼키드",
+//     profile : "./CSS/IMG/parkki.png",
+//     birthday : "19970120",
+//     type : "friends"
+// },
+// {
+//     name : "박유진",
+//     message : "여자친구 사귀고 싶다...",
+//     music : "대화 - MC스나이퍼",
+//     profile : "./CSS/IMG/default.png",
+//     birthday : "19970828",
+//     type : "friends"
+// },
+// {
+//     name : "옥경욱",
+//     message : "연애하니까 좋더랔ㅋ",
+//     music : "사랑인걸 - 모세",
+//     profile : "./CSS/IMG/ok.png",
+//     birthday : "19971201",
+//     type : "friends"
+// },
+// {
+//     name : "채은지",
+//     message : "오늘도 킹받는다",
+//     music : "범퍼카 - 한요한",
+//     profile : "./CSS/IMG/chea.png",
+//     birthday : "19971006",
+//     type : "friends"
+// },
+// {
+//     name : "최대혁",
+//     message : "인생이란.. 무엇인가",
+//     music : "신촌을못가 - 포스트맨",
+//     profile : "./CSS/IMG/choi.png",
+//     birthday : "19970713",
+//     type : "friends"
+// },
+// ]
+// const a  = document.getElementsByClassName("profile")[0];
+// console.log(a); 
+
+var modal = document.getElementById("modal");
+var button = document.getElementsByClassName("button");
+var close = document.getElementById("close");
+if(button){
+    // modal.style.display = "";
+}
